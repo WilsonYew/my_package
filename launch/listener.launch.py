aaa6908cac8644d8a498c551bbe150398753bb26
WilsonYew <wilsonyew38@gmail.com>
2025-03-23 23:00:01 +0800

    first commit update

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -5,6 +5,6 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='demo_nodes_py',
-            execute='listener'
+            executable='listener'
         )
     ])
